5f28a97151b443fcbb26d4b2496212f3ea502ae2
smoreyaks <bnixon.eng@gmail.com>
2022-08-12 00:08:22 +1000

    Slight Re-format

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,7 @@
+import { useFetch } from '../../hooks/useFetch'
+
+// Styles 
 import './Home.css'
-import { useFetch }from '../../hooks/useFetch'
-
-
-import React from 'react'
 
 export default function Home() {
 const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
@@ -12,9 +11,9 @@ const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
     <div className="home">
       { error && <p className="error">{ error }</p> }
       { isPending && <p className="loading">Loading...</p> }
-      { data && data.map(recipe => 
+      { data && data.map(recipe => (
         <h2 key={recipe.id}>{ recipe.title } </h2>
-      )}
+      ))}
     </div>
   )
 }
