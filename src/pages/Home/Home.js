cdc8b2e45505871f5da0ea521a5ab3778ab228c4
smoreyaks <bnixon.eng@gmail.com>
2022-08-15 23:41:11 +1000

    Recipe List Component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,13 @@
+import RecipeList from '../../components/RecipeList'
 import { useFetch } from '../../hooks/useFetch'
 
 // Styles 
 import './Home.css'
+
+
+// Components
+
+
 
 export default function Home() {
 const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
@@ -11,9 +17,7 @@ const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
     <div className="home">
       { error && <p className="error">{ error }</p> }
       { isPending && <p className="loading">Loading...</p> }
-      { data && data.map(recipe => (
-        <h2 key={recipe.id}>{ recipe.title } </h2>
-      ))}
+      { data && <RecipeList recipes={ data } /> }
     </div>
   )
 }
