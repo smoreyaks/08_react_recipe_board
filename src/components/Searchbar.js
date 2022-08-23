6b92fd27ffcb63ca95816d6ace0698264321b8bf
smoreyaks <bnixon.eng@gmail.com>
2022-08-23 22:47:12 +1000

    Making a Searchbar Component

---
+++ src/components/Searchbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useState } from 'react'
+
+// Styles
+
+export default function Searchbar() {
+const [term, setTerm] = useState('')
+const history = useHistory()
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    history.push(`/search?q=${term}`)
+}
+
+    return (
+        <div className="searchbar">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="search">
+                    Search:
+                </label>
+                <input 
+                    type="text" 
+                    id="search"
+                    onChange={(e) => setTerm(e.target.value)}
+                    required
+                />
+            </form>
+        </div>
+    )
+}
