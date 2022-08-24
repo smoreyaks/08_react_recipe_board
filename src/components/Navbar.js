8f7b33f56b5ecf5dd6186277e83cc94928d3c91e
smoreyaks <bnixon.eng@gmail.com>
2022-08-24 18:02:23 +1000

    Accessing Context Values

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom'
 
-// Styles
-import './Navbar.css'
+// Hooks
+import { useContext } from 'react'
 
 // Components
 import Searchbar from './Searchbar'
 
+// Context
+import { ThemeContext } from '../context/ThemeContext'
+
+// Styles
+import './Navbar.css'
+
+
 
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={ {background: color } }>
         <nav>
           <Link to="/" className="brand">
               <h1>Cooking Ninja</h1>
